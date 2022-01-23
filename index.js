3bd9a65040f00d49dc937a3e3bcf6f5552ffc451
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-01-23 11:42:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -208,16 +208,6 @@ client.on('messageCreate', async message => {
     }
 });
 
-client.on("messageCreate", async message => {
-  if (message.content.startsWith("/add -role")) {
-    const list = message.mentions.members();
-    const role = message.mentions.roles.fist();
-    await message.list.roles.add(role.id);
-  } else {
-    return;
-  };
-});
-
 //スレッド周り
 //ロールid記録
 //スレッド作成
