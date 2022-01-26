7fa72c8aba173274c31da8d253885a4b8ed34a0c
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-01-26 17:00:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -248,7 +248,31 @@ client.on("threadUpdate", async (oldThread, newThread) => {
   };
 });
 
-//記録
+//スラッシュコマンドテスト
+client.once("ready", async () => {
+    const data = [{
+        name: "ping",
+        description: "現在のping値を取得します。",
+    }];
+    await client.application.commands.set(data, '888981896594350132');
+});
 
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    if (interaction.commandName === 'ping') {
+      const pingem = {
+        "color": 16737977,
+        "fields": [
+          {
+            "name": "📡Bot反応時間",
+            "value": "現在のPing値は" + client.ws.ping + "msです。"
+          }
+        ]
+      };
+      await interaction.reply({ embeds: [pingem] })
+    }
+});
 
 client.login(process.env.token);
