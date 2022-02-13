2215073cd18098dbeab74e6d024a95ffa1b1596b
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-02-13 10:25:05 +0900

    Update index.js

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ client.on('messageCreate', async message => {
         color: 16727276,
         footer: {
           icon_url: `${msg.guild.iconURL()}`,
-          text: `${msg.guild.channels.cache.find(channel) => channel.name}・${msg.createdAt.toFormat("YYYY/MM/DD")}`
+          text: `${msg.createdAt.toFormat("YYYY/MM/DD")}`
         },
         author: {
           name: `${msg.author.username}`,
