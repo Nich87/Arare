3d13ac1bfefb06593217eb6d7c24b04e0293ef04
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-02-15 09:04:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -151,11 +151,7 @@ client.on('messageCreate', async message => {
           name: `${msg.author.username}`,
           icon_url: `${msg.author.displayAvatarURL({ format: 'png' })}`
         },
-<<<<<<< HEAD
         description: `${msg.embeds[0] == undefined ? msg.content : msg.embeds[0].description }`
-=======
-        description: msg.embeds[0] === undefined ? `${msg.content}` : `${msg.embeds[0].description}`
->>>>>>> 1cfa6c26c0ddc95839f48d0423ba06c2823c0894
       }]
     }))
     .catch(console.error);
