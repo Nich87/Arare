bc0f4a233cbdad98aaf0d0b7c396895fe4faf263
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-02-23 19:55:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const fs = require('fs');
 client.on('ready', async () => {
   client.user.setActivity(`#/help | ${client.guilds.cache.map(guild => guild.memberCount).reduce((p, c) => p + c)}人`, { type: 'PLAYING' });
   console.log(`${client.user.tag}にログインしました。`);
-    const server_id = client.guilds.server.id();
+    const server = client.guilds.server();
     const data = [{
       name: "add-role-to-everyone",
       description: "サーバーにいるメンバー全員にロールを付与します。",
@@ -23,7 +23,7 @@ client.on('ready', async () => {
       required: true,
     }],
   }];
-  await client.application.commands.set(data, `${server_id}`);
+  await client.application.commands.set(data, `${server.id}`);
 });
 
 client.on("interactionCreate", async (interaction) => {
