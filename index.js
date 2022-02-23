48839accaf4b65c92bd75e3d4ffdf8e402b34421
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-02-23 20:51:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ client.on("interactionCreate", async (interaction) => {
     }
     if (interaction.commandName === 'add-role-to-everyone') {
       const role = interaction.options.getRole('ロール');
-      interaction.channel.send(`${role.name}を全員に付与しました。`)
       interaction.guild.members.fetch()
       .then(members => Promise.all(members.map(member => member.roles.add(`${role.id}`))))
       .catch(console.error)
+      await interaction.reply(`${role.name}を全員に付与しました。`)
     }
 });
 
