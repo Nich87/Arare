50c06f442e380d8c2dd27e9d644d03c92408c392
Puton1221 <88196889+Puton1221@users.noreply.github.com>
2022-02-24 15:46:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on('ready', async () => {
    }],
  },
 ]
-  await client.application.commands.set(data);
+  await client.application.commands.set(data, `${server_id}`);
 });
 
 client.on("interactionCreate", async (interaction) => {
